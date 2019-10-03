6050922cfc94cfa9e7a2f2e35f0810ac8e174e6c
Evalyn Berleant <eberleant@gmail.com>
2019-10-03 17:32:38 -0400

    Allow swiping to open and close hamburger menu

--- script.js
+++ script.js
@@ -36,7 +36,6 @@ mNavBtn.addEventListener("click", toggleMobileMenu);
 mNavbar.style.right = -mNavbarWidth + "px";
 
 function toggleMobileMenu() {
-	
 	if (mNavbar.style.right == "0px" || mNavbar.style.right == "" || mNavbar.style.right == "0") {
 		window.requestAnimationFrame(function(){
 			mNavbar.setAttribute("style", "right: 0");
@@ -53,3 +52,45 @@ function toggleMobileMenu() {
 		});
 	}
 }
+
+
+// from https://stackoverflow.com/questions/2264072/detect-a-finger-swipe-through-javascript-on-the-iphone-and-android
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if (xDiff > 0 && mNavbar.style.right == "-" + mNavbarWidth + "px") {
+            toggleMobileMenu();
+        } else if (mNavbar.style.right == "0px" || mNavbar.style.right == "" || mNavbar.style.right == "0") {
+            toggleMobileMenu();
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
