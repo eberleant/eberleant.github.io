f638e54bd09899940728efc49114864991c074f5
Evalyn Berleant <eberleant@gmail.com>
2019-10-03 03:22:55 -0400

    Create portfolio website.

---
+++ script.js
@@ -0,0 +1,19 @@
+const navAbout = document.getElementById("nav-about");
+const navResume = document.getElementById("nav-resume");
+const navProgramming = document.getElementById("nav-programming");
+const navFooter = document.getElementById("nav-contact");
+const sectionAbout = document.getElementById("about");
+const sectionResume = document.getElementById("resume");
+const sectionProgramming = document.getElementById("programming");
+const sectionFooter = document.getElementById("footer");
+const resumeDownload = document.getElementById("resume-dl");
+const scrollOptions = {
+	behavior: "smooth",
+	block: "start",
+	inline: "nearest",
+}
+
+navAbout.addEventListener("click", e => sectionAbout.scrollIntoView(scrollOptions));
+navResume.addEventListener("click", e => sectionResume.scrollIntoView(scrollOptions));
+navProgramming.addEventListener("click", e => sectionProgramming.scrollIntoView(scrollOptions));
+navFooter.addEventListener("click", e => sectionFooter.scrollIntoView(scrollOptions));
