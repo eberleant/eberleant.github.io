a044ecba30126103bf90122b39a35b9e27bb6948
Evalyn Berleant <eberleant@gmail.com>
2021-03-14 22:36:12 -0500

    Configure eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
-} from "react-router-dom";
+  Link,
+} from 'react-router-dom';
 
 export default function App() {
   return (
